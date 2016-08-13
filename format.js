4d1fc4ba9c051e49cb8a6244100cf516c69be55d
Christopher Almeida <run.revelations@gmail.com>
2016-08-12 17:02:18 -0700

    Update format.js

--- format.js
+++ format.js
@@ -1,7 +1,7 @@
-function formatDate(input){
+function formatDate(input, opts){
    var epoch = new Date(0);
    epoch.setSeconds(parseInt(input));
-   epoch.setHours(epoch.getHours() + 1);
+   // epoch.setHours(epoch.getHours() + 1); hour offset
    var date = epoch.toISOString();
    date = date.replace('T', ' ');
    return date.split('.')[0].split(' ')[0] + ' ' + epoch.toLocaleTimeString().split(' ')[0];
